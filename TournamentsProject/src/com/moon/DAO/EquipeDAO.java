08b46ab73c41cd97b669d91e38cbc2fb1ca2069a
Jonatas Coutinho <jonatascoutinho10@gmail.com>
2020-06-14 18:11:40 -0300

    campeonatos da equipe

--- TournamentsProject/src/com/moon/DAO/EquipeDAO.java
+++ TournamentsProject/src/com/moon/DAO/EquipeDAO.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 
 import com.moon.BEAN.CampeonatoBean;
 import com.moon.BEAN.EquipeBean;
+import com.moon.BO.CampeonatoBO;
 import com.moon.DAO.ConnectionFactory;
 
 import jdk.nashorn.internal.runtime.ListAdapter;
@@ -87,22 +88,36 @@ public class EquipeDAO {
 		return null;
 	}
 	
-	public ArrayList<Integer> buscarCampeonatos(EquipeBean equipe) {
+	public ArrayList<CampeonatoBean> buscarCampeonatos(EquipeBean equipe) {
 		
-		ArrayList<Integer> campeonatos = new ArrayList<Integer>();
+		ArrayList<Integer> idCampeonatos = new ArrayList<Integer>();
 				
 		String sql = "SELECT * FROM campeonato_equipe WHERE id_equipe='"+equipe.getId()+"'";
+		
+		
+		CampeonatoBO campeonatoBO = new CampeonatoBO();
+		ArrayList<CampeonatoBean> campeonatos = campeonatoBO.listarCampeonatos();
+		ArrayList<CampeonatoBean> resultado = new ArrayList<CampeonatoBean>();
 		
 		try {
 			st = conn.createStatement();
 			rs = st.executeQuery(sql);
 
 			if(rs.next()) {
-				campeonatos.add(rs.getInt("id_campeonato"));
+				idCampeonatos.add(rs.getInt("id_campeonato"));
 			}
-			return campeonatos;
+			
+			for(int i = 0; i < idCampeonatos.size(); i++) {
+				for(int j = 0; j < campeonatos.size(); j++) {
+					if(idCampeonatos.get(i) == campeonatos.get(j).getId()){
+						resultado.add(campeonatos.get(j));
+					}
+				}
+			}
+			
+			return resultado;
 		}catch (Exception error) {
-			return campeonatos;
+			return resultado;
 		}
 	}
 }
